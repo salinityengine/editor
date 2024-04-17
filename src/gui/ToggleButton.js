b1bf174c355b9d21417628478c952ce82bdcbdfc
Stephens Nunnally <stevinz@gmail.com>
2024-04-17 15:26:35 -0700

    Fix toggle button

--- src/gui/ToggleButton.js
+++ src/gui/ToggleButton.js
@@ -2,27 +2,36 @@ import * as SUEY from 'gui';
 import { Config } from '../config/Config.js';
 import { Signals } from '../config/Signals.js';
 
-class ToggleButton extends SUEY.ToolbarButton {
+class ToggleButton extends SUEY.Button {
 
     constructor(icon = '', tip, configKey, callback = () => {}) {
-        super(null, null, false /* addBackground */, false /* closesMenus? */);
+        super(null, false /* closesMenus */);
         const self = this;
-        this.addClass('salt-toggle-button');
-        this.add(new SUEY.VectorBox(icon));
+        this.setClass('suey-toolbar-button', 'salt-toggle-button');
+        this.setStyle('pointerEvents', 'all');
         if (tip) this.setAttribute('tooltip', tip);
 
+        // Contents
+        const buttonImageHolder = new SUEY.ShadowBox().setStyle('pointer-events', 'none');
+        if (icon && icon !== '') buttonImageHolder.add(new SUEY.VectorBox(icon));
+        this.add(buttonImageHolder);
+
+        this.contents = function() { return buttonImageHolder };
+
+        // Events
+        this.on('pointerdown', (event) => { event.stopPropagation(); });
+        this.on('pointerup', (event) => { event.stopPropagation(); });
+
+        // Toggle
         function setButtonValue() {
             self.removeClass('suey-toggled');
             if (configKey && Config.getKey(configKey)) self.addClass('suey-toggled');
         }
-
-        // Toggle
         this.onPress(() => {
             if (configKey) Config.setKey(configKey, (!Config.getKey(configKey)));
             setButtonValue();
             if (typeof callback === 'function') callback();
         });
-
         Signals.connect(this, 'settingsRefreshed', setButtonValue)
         setButtonValue();
     }
